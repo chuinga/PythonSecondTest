4a2a0915fe3c0db751e6455b230c16cf98c7309b
chuinga <mfproencamartins@gmail.com>
2024-08-15 19:02:26 +0100

    feat: add variables and convert str to int and vice-versa

---
+++ main.py
@@ -0,0 +1,25 @@
+"""item = 'Banana'
+Item = 'Apple'
+item_name = 'Orange'
+
+print (item, Item)
+
+print('Hello ' + item_name)"""
+
+"""integer = 2024
+word = 'text'
+isHappy = False
+naughty_list = ['Luigi', 'Mario', 'Toad']
+
+print(integer)
+print(word)
+print(isHappy)
+print(naughty_list)"""
+
+"""integer = 2024
+name = 'Mario'
+string_number = '8'
+
+print(name + str(integer))
+print(integer + int(string_number))"""
+
