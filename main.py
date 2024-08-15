1c9707b3240bc2b12aac9c6d167b8eaf8fdaaf51
chuinga <mfproencamartins@gmail.com>
2024-08-15 19:31:14 +0100

    feat: add conditions and for loop

--- main.py
+++ main.py
@@ -23,3 +23,41 @@ string_number = '8'
 print(name + str(integer))
 print(integer + int(string_number))"""
 
+"""a = 10
+b = 5
+
+addition = a + b
+difference = a - b
+multiply = a * b
+divide = a / b
+power = a ** b
+
+print(addition)
+print(difference)
+print(multiply)
+print(divide)
+print(power)"""
+
+"""age = 7
+isHappy = True
+
+if age >= 21:
+    print('You are a adult')
+elif age >= 13:
+    print('You are a teenager')
+else:
+    print('You are a kid')
+
+if isHappy:
+    print('You are a happy')
+else:
+    print('You are not happy')"""
+
+
+"""isHappy = True
+
+print('You are happy') if isHappy else print('You are not happy')"""
+
+for i in range(10):
+    print('Hello', i + 1)
+
